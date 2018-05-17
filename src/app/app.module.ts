4b704ed91013da52c28204b5b85ac98ff5dccd15
omar <omar.rodriguez@mexmax-internet.com>
2018-05-17 18:31:23 -0500

    added roulette component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RouletteComponent } from './roulette/roulette.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouletteComponent
   ],
   imports: [
     BrowserModule
